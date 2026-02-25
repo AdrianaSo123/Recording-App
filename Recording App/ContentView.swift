109aed2a9d9434149d60b830817aafa0f4b6a382
AdrianaSo123 <ams328@njit.edu>
2026-02-25 16:01:30 -0500

    Initial Commit

---
+++ Recording App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Recording App
+//
+//  Created by Adriana So on 2/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Recording App/Recording_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recording_AppApp.swift
+//  Recording App
+//
+//  Created by Adriana So on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct Recording_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
